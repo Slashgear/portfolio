e08c8f65af92b871dd659f19390f1bc426711f40
Slashgear <antoine395.caron@laposte.net>
2015-07-16 11:44:03 +0200

    Update translation wordings

---
+++ public/contact-form.php
@@ -0,0 +1,63 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once '../phpmailer/PHPMailerAutoload.php';
+
+if (isset($_POST['inputName']) && isset($_POST['inputEmail']) && isset($_POST['inputSubject']) && isset($_POST['inputMessage'])) {
+
+    //check if any of the inputs are empty
+    if (empty($_POST['inputName']) || empty($_POST['inputEmail']) || empty($_POST['inputSubject']) || empty($_POST['inputMessage'])) {
+        $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+        echo json_encode($data);
+        exit;
+    }
+
+    //create an instance of PHPMailer
+    $mail = new PHPMailer();
+    //Tell PHPMailer to use SMTP
+    $mail->isSMTP();
+    $ini_array = parse_ini_file("../dev.ini");
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+    //Ask for HTML-friendly debug output
+    $mail->Debugoutput = 'html';
+    //Set the hostname of the mail server
+    $mail->Host = 'smtp.gmail.com';
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = $ini_array['user'];
+    //Password to use for SMTP authentication
+    $mail->Password = $ini_array['pswd'];
+    $mail->From = $_POST['inputEmail'];
+    $mail->FromName = $_POST['inputName'];
+    $mail->AddAddress('antoine395.caron@gmail.com'); //recipient
+    $mail->Subject = $_POST['inputSubject']." PORTFOLIO";
+    $mail->Body = "Name: " . $_POST['inputName'] . "\r\n\r\nMessage: " . stripslashes($_POST['inputMessage']);
+
+    if (isset($_POST['ref'])) {
+        $mail->Body .= "\r\n\r\nRef: " . $_POST['ref'];
+    }
+
+    if(!$mail->send()) {
+        $data = array('success' => false, 'message' => 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
+        echo json_encode($data);
+        exit;
+    }
+
+    $data = array('success' => true, 'message' => 'Thanks! We have received your message.');
+    echo json_encode($data);
+
+} else {
+
+    $data = array('success' => false, 'message' => 'Please fill out the form completely.');
+    echo json_encode($data);
+
+}
--- public/core.js
+++ public/core.js
@@ -1,20 +1,112 @@
 
 angular.module('jm.i18next').config(['$i18nextProvider', function ($i18nextProvider) {
+    var lang=navigator.language;
     $i18nextProvider.options = {
-        lng: 'fr',
-        useCookie: false,
+        lng: lang,
+        useCookie: true,
         useLocalStorage: false,
         fallbackLng: 'fr',
-        resGetPath: '/public/locales/__lng__/__ns__.json',
+        resGetPath: 'public/locales/__lng__/__ns__.json',
         defaultLoadingValue: '' // ng-i18next option, *NOT* directly supported by i18next
     };
 }]);
 
-var portfolio = angular.module('portfolio',['jm.i18next']);
+var portfolio = angular.module('portfolio',['jm.i18next','ngRoute','ngSanitize','angular-google-analytics'])
+.config(function(AnalyticsProvider) {
+        AnalyticsProvider.setAccount('UA-XXXXX-xx');
+        AnalyticsProvider.trackPages(true);
+        AnalyticsProvider.trackUrlParams(true);
 
-portfolio.controller('mainController',function ($scope,$i18next) {
+        // Optional set domain (Use 'none' for testing on localhost)
+        AnalyticsProvider.setDomainName('XXX');
+        AnalyticsProvider.useDisplayFeatures(true);
+        // url prefix (default is empty)
+        // - for example: when an app doesn't run in the root directory
+        AnalyticsProvider.trackPrefix('my-application');
+        AnalyticsProvider.useAnalytics(true);
+        AnalyticsProvider.ignoreFirstPageLoad(true);
+        AnalyticsProvider.useEnhancedLinkAttribution(true);
+        AnalyticsProvider.setPageEvent('$stateChangeSuccess');
+        AnalyticsProvider.delayScriptTag(true);
+}).run(function(Analytics) {
+      // For automatic page tracking
+});
+
+portfolio.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: "mainController",
+      templateUrl: "public/views/home.html"
+    })
+    .when('/cursus', {
+      controller: "cursusController",
+      templateUrl: "public/views/cursus.html"
+    })
+    .when('/projects', {
+      controller: "projectsController",
+      templateUrl: "public/views/projects.html"
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+portfolio.controller('mainController', function($scope,$i18next) {
   angular.element(document).ready(function () {
       $.material.init();
-      $i18next.options.lng='fr';
   });
+  $scope.setEn = function() {
+    $i18next.options.lng = 'en';
+  };
+  $scope.setFr = function() {
+    $i18next.options.lng = 'fr';
+  };
 });
+
+var param = function(data) {
+  var returnString = '';
+  for (var d in data){
+    if (data.hasOwnProperty(d)){
+    returnString += d + '=' + data[d] + '&';}
+  }
+  return returnString.slice( 0, returnString.length - 1 );
+};
+
+portfolio.controller('contactController',function ($scope,$http) {
+  $scope.result = 'hidden';
+   $scope.resultMessage;
+   $scope.formData; //formData is an object holding the name, email, subject, and message
+   $scope.submitButtonDisabled = false;
+   $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
+   $scope.submit = function(contactform, e) {
+       $scope.submitted = true;
+       $scope.submitButtonDisabled = true;
+       $http({
+           method  : 'POST',
+           url     : 'public/contact-form.php',
+           data    : param($scope.formData),  //param method from jQuery
+           headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+       }).success(function(data){
+           var answer=data.substring(data.lastIndexOf("{"),data.lastIndexOf("}")+1);
+           answer=JSON.parse(answer);
+           if (answer) { //success comes from the return json object
+               $scope.submitButtonDisabled = true;
+               $scope.resultMessage = answer.message;
+               $scope.result='alert alert-dismissable alert-success';
+           } else {
+               $scope.submitButtonDisabled = false;
+               $scope.resultMessage = answer.message;
+               $scope.result='alert alert-dismissable alert-danger';
+           }
+       });
+      //e.preventDefault();
+   }
+});
+
+portfolio.controller('cursusController',function ($scope,$i18next) {
+
+});
+
+portfolio.controller('projectsController',function ($scope,$i18next) {
+
+})
